999374a4675f758265c00ac24408cf9a48e099c7
June Tate-Gans <jtgans@google.com>
2020-10-22 20:27:13 +0000

    kata: Fix the build scripts to utilize CAmkES
    Since we updated to CAmkES, kata has been unable to build using the
    typical m scripts.
    
    Change-Id: I210bbc84ad8d72d9e0395d9b8b1a31368ede5159
    GitOrigin-RevId: 332f40b4bbe1466282c80d2f9e0d21bd3a3fbd7e

--- kata.mk
+++ kata.mk
@@ -1,17 +1,16 @@
-KATA_CMAKE_ARGS := -DCROSS_COMPILER_PREFIX=riscv32-unknown-elf- \
-                   -DCMAKE_BUILD_TYPE=Release \
-                   -DCMAKE_TOOLCHAIN_FILE=$(ROOTDIR)/kata/kernel/gcc.cmake \
-                   -DSEL4_CACHE_DIR=$(OUT)/kata/sel4_cache \
-                   -G Ninja
+KATA_INIT_ARGS := -DCROSS_COMPILER_PREFIX=riscv32-unknown-elf- \
+                  -DCMAKE_BUILD_TYPE=Release \
+                  -DPLATFORM=spike \
+                  -DRISCV32=1
 
 KATA_TOPLEVEL_NINJA_SCRIPT := $(OUT)/kata/build.ninja
 KATA_ROOTSERVER_IMAGE_NAME := $(OUT)/kata/images/ProcessManager-image-riscv-spike
 KATA_SIMULATE_SCRIPT_NAME  := $(OUT)/kata/simulate
 
-KATA_SOURCES := $(shell find $(ROOTDIR)/kata -type f)
+KATA_SOURCES := $(shell find $(ROOTDIR)/kata -name \*.c -name \*.h -name \*.cpp -type f)
 
 $(KATA_TOPLEVEL_NINJA_SCRIPT): $(KATA_SOURCES) | $(OUT)/kata
-	pushd $(OUT)/kata; cmake $(KATA_CMAKE_ARGS) $(ROOTDIR)/kata
+	pushd $(OUT)/kata; $(ROOTDIR)/kata/init-build.sh $(KATA_INIT_ARGS)
 
 $(KATA_ROOTSERVER_IMAGE_NAME): $(KATA_TOPLEVEL_NINJA_SCRIPT) $(KATA_SOURCES) | $(OUT)/kata
 	pushd $(OUT)/kata; ninja
