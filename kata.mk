1977b959793dcb7368530a969e555c8d1f55d53b
Matt Harvey <mattharvey@google.com>
2021-02-02 14:11:41 -0800

    Changes kata makefile to build processmanager by default
    Change-Id: I147c1e68d5f018a3178e8b536dfffdaa93e2bfa8
    GitOrigin-RevId: 63ad7a4cf448a650fbe1e2933020aba6b0c79eec

--- kata.mk
+++ kata.mk
@@ -1,16 +1,13 @@
-KATA_INIT_ARGS := -DCROSS_COMPILER_PREFIX=riscv32-unknown-elf- \
-                  -DCMAKE_BUILD_TYPE=Release \
-                  -DPLATFORM=spike \
-                  -DRISCV32=1
+KATA_INIT_ARGS := -DCROSS_COMPILER_PREFIX=riscv32-unknown-elf-
 
 KATA_TOPLEVEL_NINJA_SCRIPT := $(OUT)/kata/build.ninja
-KATA_ROOTSERVER_IMAGE_NAME := $(OUT)/kata/images/ProcessManager-image-riscv-spike
+KATA_ROOTSERVER_IMAGE_NAME := $(OUT)/kata/images/capdl-loader-image-riscv-spike
 KATA_SIMULATE_SCRIPT_NAME  := $(OUT)/kata/simulate
 
 KATA_SOURCES := $(shell find $(ROOTDIR)/kata -name \*.c -name \*.h -name \*.cpp -type f)
 
 $(KATA_TOPLEVEL_NINJA_SCRIPT): $(KATA_SOURCES) | $(OUT)/kata
-	pushd $(OUT)/kata; $(ROOTDIR)/kata/init-build.sh $(KATA_INIT_ARGS)
+	pushd $(OUT)/kata; cmake -G Ninja $(KATA_INIT_ARGS) $(ROOTDIR)/kata/projects/processmanager
 
 $(KATA_ROOTSERVER_IMAGE_NAME): $(KATA_TOPLEVEL_NINJA_SCRIPT) $(KATA_SOURCES) | $(OUT)/kata
 	pushd $(OUT)/kata; ninja
