bf4ab215a1302c8d70aada7a7063ceb1dc311032
Cindy Liu <hcindyl@google.com>
2021-03-15 17:08:42 -0700

    Add IREE toolchain to toolchain.mk
    Add IREE's riscv-gcc and LLVM toolchain into toolchain.mk. It runs on
    64-bit linux configuration, but will eventually be updated to 32-bit
    bare-metal.
    
    Add 64-bit Qemu simulator to simulate IREE executable.
    
    Change-Id: Ibde3c53aefa06d522b143f5cd8295ffb9feafcc6
    GitOrigin-RevId: b96e078ab693b48c410c1f1c862d4a86263c8094

--- toolchain.mk
+++ toolchain.mk
@@ -6,6 +6,14 @@ TOOLCHAIN_OUT_DIR   := $(CACHE)/toolchain
 TOOLCHAINVP_BUILD_DIR := $(OUT)/tmp/toolchain_vp
 TOOLCHAINVP_OUT_DIR   := $(CACHE)/toolchain_vp
 
+# TODO(hcindyl): Use toolchain_vp when 32-bit baremetal target is ready
+TOOLCHAINIREE_BUILD_DIR  := $(OUT)/tmp/toolchain_iree
+TOOLCHAINIREE_OUT_DIR    := $(CACHE)/toolchain_iree
+
+TOOLCHAINLLVM_SRC_DIR        := $(ROOTDIR)/toolchain/llvm-project
+TOOLCHAINLLVM_BUILD_DIR      := $(OUT)/tmp/toolchain_llvm
+
+
 QEMU_SRC_DIR          := $(ROOTDIR)/toolchain/riscv-qemu
 QEMU_OUT_DIR          := $(OUT)/host/qemu
 QEMU_BINARY           := $(QEMU_OUT_DIR)/riscv32-softmmu/qemu-system-riscv32
@@ -43,6 +51,41 @@ $(TOOLCHAINVP_OUT_DIR): | $(TOOLCHAIN_SRC_DIR)
 
 toolchain_vp: $(TOOLCHAINVP_OUT_DIR)
 
+# IREE toolchain
+# TODO(hcindyl): This will eventually be combined with toolchain_vp
+$(TOOLCHAINIREE_OUT_DIR): | $(TOOLCHAIN_SRC_DIR)
+	mkdir -p $(TOOLCHAINIREE_BUILD_DIR);
+	cd $(TOOLCHAINIREE_BUILD_DIR) && $(TOOLCHAIN_SRC_DIR)/configure \
+		--srcdir=$(TOOLCHAIN_SRC_DIR) \
+		--prefix=$(TOOLCHAINIREE_OUT_DIR) \
+		--with-arch=rv64gc \
+		--with-abi=lp64d \
+		--with-cmodel=medany
+	make -C $(TOOLCHAINIREE_BUILD_DIR) clean linux
+
+toolchain_iree: $(TOOLCHAINIREE_OUT_DIR)
+
+# Build with 64-bit linux config.
+# TODO(hcindyl): Move to 32-bit baremetal config
+$(TOOLCHAINLLVM_BUILD_DIR): | $(TOOLCHAINIREE_OUT_DIR)
+	cmake -B $(TOOLCHAINLLVM_BUILD_DIR) \
+		-DCMAKE_INSTALL_PREFIX=$(TOOLCHAINIREE_OUT_DIR) \
+		-DCMAKE_C_COMPILER=clang  -DCMAKE_CXX_COMPILER=clang++ \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DLLVM_TARGETS_TO_BUILD="RISCV" \
+		-DLLVM_ENABLE_PROJECTS="clang"  \
+		-DLLVM_DEFAULT_TARGET_TRIPLE="riscv64-unknown-linux-gnu" \
+		-DLLVM_INSTALL_TOOLCHAIN_ONLY=On \
+		-DDEFAULT_SYSROOT=../sysroot \
+		-G Ninja \
+		$(TOOLCHAINLLVM_SRC_DIR)/llvm
+	cmake --build $(TOOLCHAINLLVM_BUILD_DIR) --target install
+
+toolchain_llvm: $(TOOLCHAINLLVM_BUILD_DIR)
+
+toolchain_llvm_clean:
+	rm -rf $(TOOLCHAINLLVM_BUILD_DIR) $(TOOLCHAINIREE_BUILD_DIR) $(TOOLCHAINIREE_OUT_DIR)
+
 QEMU_DEPS=$(shell find $(QEMU_SRC_DIR) \( -name '*.c' -o -name '*.h' \) -printf "%p ")
 
 $(QEMU_OUT_DIR): | $(QEMU_SRC_DIR)
@@ -50,7 +93,7 @@ $(QEMU_OUT_DIR): | $(QEMU_SRC_DIR)
 
 $(QEMU_BINARY): $(QEMU_DEPS) | $(QEMU_OUT_DIR)
 	cd $(QEMU_OUT_DIR) && $(QEMU_SRC_DIR)/configure \
-		--target-list=riscv32-softmmu
+		--target-list=riscv32-softmmu,riscv64-linux-user
 	make -C $(QEMU_OUT_DIR) -j$(nproc --ignore 2)
 
 qemu: $(QEMU_BINARY)
@@ -61,4 +104,4 @@ toolchain_clean:
 qemu_clean:
 	rm -rf $(QEMU_OUT_DIR)
 
-.PHONY:: toolchain toolchain_vp qemu toolchain_clean qemu_clean
+.PHONY:: toolchain toolchain_vp qemu toolchain_clean qemu_clean toolchain_llvm
