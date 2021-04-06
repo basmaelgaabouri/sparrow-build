3926c2a59540d3f7a31daceecbca7503606e6a7c
Henry Herman <henryherman@google.com>
2021-04-06 05:40:03 +0000

    Add target for building libtock-rs demos for opentitan.
    Change-Id: I03264e053111dc79680de7af350cb05b1d528166
    GitOrigin-RevId: ac9a1d3763eb6007bc446e95298bb9fc37139ea0

--- tock.mk
+++ tock.mk
@@ -4,6 +4,10 @@ EARLGREY_TARGET_ELF=$(TOCK_OUTPUT_DIRECTORY)/riscv32imc-unknown-none-elf/release
 TOCK_DIRECTORY=sw/tock
 EARLGREY_BOARD_DIRECTORY=$(TOCK_DIRECTORY)/boards/earlgrey-nexysvideo
 
+LIBTOCKRS_DIRECTORY=sw/libtock-rs
+LIBTOCKRS_OUT=${OUT}/libtock-rs/
+
+
 TOCK_DEPS=$(shell find $(ROOTDIR)/$(TOCK_DIRECTORY) \( -name '*.rs' -o -name '*.c' \) -printf "%p ")
 
 $(EARLGREY_TARGET_ELF): $(TOCK_DEPS)
@@ -14,6 +18,14 @@ earlgrey_tock: $(EARLGREY_TARGET_ELF)
 
 earlgrey_tock_clean:
 	cd $(EARLGREY_BOARD_DIRECTORY); \
-	    make TARGET_DIRECTORY=$(TOCK_OUTPUT_DIRECTORY) V=1 clean
+		make TARGET_DIRECTORY=$(TOCK_OUTPUT_DIRECTORY) V=1 clean
 
-.PHONY:: earlgrey_tock earlgrey_tock_clean
+libtockrs_demos:
+	cd ${LIBTOCKRS_DIRECTORY}; \
+		 PLATFORM=opentitan cargo build --release --target=riscv32imc-unknown-none-elf \
+		 	--examples -p libtock -p libtock_core --target-dir=${LIBTOCKRS_OUT}
+
+libtockrs_demos_clean:
+	rm -rf ${LIBTOCKRS_OUT}
+
+.PHONY:: earlgrey_tock earlgrey_tock_clean libtockrs_demos libtockrs_demos_clean
