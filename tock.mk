ec01b384374b8eac748fafd5a8c91be438f628b9
Henry Herman <henryherman@google.com>
2021-04-07 22:14:30 +0000

    Merge "Modify tock build commands for tock release 1.6."
    GitOrigin-RevId: 99f78d83c488e99a89dd683877fce0cbc0be20d1

--- tock.mk
+++ tock.mk
@@ -1,31 +1,38 @@
 
-TOCK_OUTPUT_DIRECTORY=${OUT}/tock/
-EARLGREY_TARGET_ELF=$(TOCK_OUTPUT_DIRECTORY)/riscv32imc-unknown-none-elf/release/earlgrey-nexysvideo.elf
+TOCK_OUTPUT_DIRECTORY=$(OUT)/tock/
+EARLGREY_TARGET_ELF=$(TOCK_OUTPUT_DIRECTORY)/riscv32imc-unknown-none-elf/release/opentitan.elf
 TOCK_DIRECTORY=sw/tock
-EARLGREY_BOARD_DIRECTORY=$(TOCK_DIRECTORY)/boards/earlgrey-nexysvideo
+EARLGREY_BOARD_DIRECTORY=$(TOCK_DIRECTORY)/boards/opentitan
 
 LIBTOCKRS_DIRECTORY=sw/libtock-rs
-LIBTOCKRS_OUT=${OUT}/libtock-rs/
+LIBTOCKRS_OUT=$(OUT)/libtock-rs/
 
-
-TOCK_DEPS=$(shell find $(ROOTDIR)/$(TOCK_DIRECTORY) \( -name '*.rs' -o -name '*.c' \) -printf "%p ")
 
 $(EARLGREY_TARGET_ELF): $(TOCK_DEPS)
+
+earlgrey_tock:
 	cd $(EARLGREY_BOARD_DIRECTORY); \
 	    make BOARD_CONFIGURATION=sim_verilator TARGET_DIRECTORY=$(TOCK_OUTPUT_DIRECTORY) V=1
-
-earlgrey_tock: $(EARLGREY_TARGET_ELF)
 
 earlgrey_tock_clean:
 	cd $(EARLGREY_BOARD_DIRECTORY); \
 		make TARGET_DIRECTORY=$(TOCK_OUTPUT_DIRECTORY) V=1 clean
 
-libtockrs_demos:
-	cd ${LIBTOCKRS_DIRECTORY}; \
-		 PLATFORM=opentitan cargo build --release --target=riscv32imc-unknown-none-elf \
-		 	--examples -p libtock -p libtock_core --target-dir=${LIBTOCKRS_OUT}
+libtockrs_helloworld: earlgrey_tock
+	cd $(LIBTOCKRS_DIRECTORY); \
+		 PLATFORM=opentitan cargo run --release --target=riscv32imc-unknown-none-elf \
+		 	--example=hello_world --target-dir=$(LIBTOCKRS_OUT)
+	 riscv32-unknown-elf-objcopy --update-section .apps=$(LIBTOCKRS_OUT)/riscv32imc-unknown-none-elf/tab/opentitan/hello_world/rv32imc.tbf \
+	 	$(EARLGREY_TARGET_ELF)
 
-libtockrs_demos_clean:
-	rm -rf ${LIBTOCKRS_OUT}
+libtockrs_helloworld_clean:
+	cd $(LIBTOCKRS_DIRECTORY); \
+		 PLATFORM=opentitan cargo clean --release --target=riscv32imc-unknown-none-elf \
+		 	--target-dir=$(LIBTOCKRS_OUT)
 
-.PHONY:: earlgrey_tock earlgrey_tock_clean libtockrs_demos libtockrs_demos_clean
+tock: libtockrs_helloworld
+
+tock_clean: libtockrs_helloworld_clean earlgrey_tock_clean
+
+
+.PHONY:: earlgrey_tock earlgrey_tock_clean libtockrs_helloworld libtockrs_helloworld_clean tock
