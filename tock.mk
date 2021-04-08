1bc5ee41eca97cb87945468e3140a4230b33e1a5
Henry Herman <henryherman@google.com>
2021-04-08 05:53:47 +0000

    Modify tock targets to use matcha board and chip project.
    Change-Id: Iaba35f5200f26269cc412d2cdb9b4115b7424a6c
    GitOrigin-RevId: cc3dd6a173698b86bc1249912513788954661b9a

--- tock.mk
+++ tock.mk
@@ -1,29 +1,26 @@
 
 TOCK_OUTPUT_DIRECTORY=$(OUT)/tock/
-EARLGREY_TARGET_ELF=$(TOCK_OUTPUT_DIRECTORY)/riscv32imc-unknown-none-elf/release/opentitan.elf
-TOCK_DIRECTORY=sw/tock
-EARLGREY_BOARD_DIRECTORY=$(TOCK_DIRECTORY)/boards/opentitan
+MATCHA_TARGET_ELF=$(TOCK_OUTPUT_DIRECTORY)/riscv32imc-unknown-none-elf/release/opentitan-matcha.elf
+MATCHA_BOARD_DIRECTORY=$(ROOTDIR)/sw/tock_matcha/boards/opentitan-matcha
 
 LIBTOCKRS_DIRECTORY=sw/libtock-rs
 LIBTOCKRS_OUT=$(OUT)/libtock-rs/
 
 
-$(EARLGREY_TARGET_ELF): $(TOCK_DEPS)
+matcha_tock:
+	cd $(MATCHA_BOARD_DIRECTORY); \
+	    make BOARD_CONFIGURATION=sim_verilator TARGET_DIRECTORY=$(TOCK_OUTPUT_DIRECTORY)
 
-earlgrey_tock:
-	cd $(EARLGREY_BOARD_DIRECTORY); \
-	    make BOARD_CONFIGURATION=sim_verilator TARGET_DIRECTORY=$(TOCK_OUTPUT_DIRECTORY) V=1
+matcha_tock_clean:
+	cd $(MATCHA_BOARD_DIRECTORY); \
+		make TARGET_DIRECTORY=$(TOCK_OUTPUT_DIRECTORY) clean
 
-earlgrey_tock_clean:
-	cd $(EARLGREY_BOARD_DIRECTORY); \
-		make TARGET_DIRECTORY=$(TOCK_OUTPUT_DIRECTORY) V=1 clean
-
-libtockrs_helloworld: earlgrey_tock
+libtockrs_helloworld: matcha_tock
 	cd $(LIBTOCKRS_DIRECTORY); \
 		 PLATFORM=opentitan cargo run --release --target=riscv32imc-unknown-none-elf \
 		 	--example=hello_world --target-dir=$(LIBTOCKRS_OUT)
 	 riscv32-unknown-elf-objcopy --update-section .apps=$(LIBTOCKRS_OUT)/riscv32imc-unknown-none-elf/tab/opentitan/hello_world/rv32imc.tbf \
-	 	$(EARLGREY_TARGET_ELF)
+	 	$(MATCHA_TARGET_ELF)
 
 libtockrs_helloworld_clean:
 	cd $(LIBTOCKRS_DIRECTORY); \
@@ -32,7 +29,7 @@ libtockrs_helloworld_clean:
 
 tock: libtockrs_helloworld
 
-tock_clean: libtockrs_helloworld_clean earlgrey_tock_clean
+tock_clean: libtockrs_helloworld_clean matcha_tock_clean
 
 
-.PHONY:: earlgrey_tock earlgrey_tock_clean libtockrs_helloworld libtockrs_helloworld_clean tock
+.PHONY:: matcha_tock matcha_tock_clean libtockrs_helloworld libtockrs_helloworld_clean tock
