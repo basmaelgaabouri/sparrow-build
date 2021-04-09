0891bcca8fd8a91fa91b5bb6fed636bc1fc18978
Henry Herman <henryherman@google.com>
2021-04-09 04:04:09 +0000

    Use ABI without float/double support.
    * Fix error where variable names were reused/reassigned.
    
    Change-Id: Ie59da7815e699386fc16c0abda0e000ac56e4148
    GitOrigin-RevId: 639ecb62f7d0df0d59a2b76454bd2aa1e8346a22

--- sparrow_boot_rom.mk
+++ sparrow_boot_rom.mk
@@ -1,35 +1,36 @@
 SHODAN_BOOT_ROM_BUILD_DIR=$(OUT)/sparrow_boot_rom
-SHODAN_BUILD_OUT_DIR=$(SHODAN_BOOT_ROM_BUILD_DIR)/build-out
-SHODAN_BUILD_NINJA_SCRIPT=$(SHODAN_BOOT_ROM_BUILD_DIR)/build-out/build.ninja
-SHODAN_BUILD_TOOLCHAIN_CONFIG=$(SHODAN_BOOT_ROM_BUILD_DIR)/toolchain-configured.txt
-SHODAN_MANIFEST=$(SHODAN_BUILD_OUT_DIR)/sw_sparrow/device/rom_exts/manifest.h
-SHODAN_DEPS=$(SHODAN_MANIFEST) $(SHODAN_BUILD_NINJA_SCRIPT)
+SHODAN_BOOT_ROM_BUILD_OUT_DIR=$(SHODAN_BOOT_ROM_BUILD_DIR)/build-out
+SHODAN_BOOT_ROM_BUILD_NINJA_SCRIPT=$(SHODAN_BOOT_ROM_BOOT_ROM_BUILD_DIR)/build-out/build.ninja
+SHODAN_BOOT_ROM_BUILD_TOOLCHAIN_CONFIG=$(SHODAN_BOOT_ROM_BUILD_DIR)/toolchain-configured.txt
+SHODAN_BOOT_ROM_MANIFEST=$(SHODAN_BOOT_ROM_BUILD_OUT_DIR)/sw_sparrow/device/rom_exts/manifest.h
+SHODAN_BOOT_ROM_DEPS=$(SHODAN_BOOT_ROM_MANIFEST) $(SHODAN_BOOT_ROM_BUILD_NINJA_SCRIPT)
 
 $(SHODAN_BOOT_ROM_BUILD_DIR):
 	@mkdir -p "$(SHODAN_BOOT_ROM_BUILD_DIR)"
 
-$(SHODAN_BUILD_TOOLCHAIN_CONFIG): | $(SHODAN_BOOT_ROM_BUILD_DIR)
-	@echo "Setup toolchain configuration $(SHODAN_BUILD_TOOLCHAIN_CONFIG)"
+$(SHODAN_BOOT_ROM_BUILD_TOOLCHAIN_CONFIG): | $(SHODAN_BOOT_ROM_BUILD_DIR)
+	@echo "Setup toolchain configuration $(SHODAN_BOOT_ROM_BUILD_TOOLCHAIN_CONFIG)"
 	cd $(ROOTDIR)/hw/opentitan; \
-	    cp toolchain.txt "$(SHODAN_BUILD_TOOLCHAIN_CONFIG)"
+	    cp toolchain.txt "$(SHODAN_BOOT_ROM_BUILD_TOOLCHAIN_CONFIG)"
 	cd $(ROOTDIR)/hw/opentitan; \
-	    sed -i "s#/tools/riscv/bin#$(CACHE)/toolchain/bin#g;s#medany#medlow#g" "$(SHODAN_BUILD_TOOLCHAIN_CONFIG)"
+	    sed -i "s#/tools/riscv/bin#$(CACHE)/toolchain/bin#g;" "$(SHODAN_BOOT_ROM_BUILD_TOOLCHAIN_CONFIG)"; \
+	    sed -i "s#medany#medlow#g" "$(SHODAN_BOOT_ROM_BUILD_TOOLCHAIN_CONFIG)"
 
-$(SHODAN_BUILD_NINJA_SCRIPT): $(SHODAN_BUILD_TOOLCHAIN_CONFIG)
-	@echo "Creating build directories $(SHODAN_BUILD_OUT_DIR)"
+$(SHODAN_BOOT_ROM_BUILD_NINJA_SCRIPT): $(SHODAN_BOOT_ROM_BUILD_TOOLCHAIN_CONFIG)
+	@echo "Creating build directories $(SHODAN_BOOT_ROM_BUILD_OUT_DIR)"
 	cd $(ROOTDIR)/hw/opentitan; \
-	    BUILD_ROOT=$(SHODAN_BOOT_ROM_BUILD_DIR) ./meson_init.sh -f -t "$(SHODAN_BUILD_TOOLCHAIN_CONFIG)";
+	    BUILD_ROOT=$(SHODAN_BOOT_ROM_BUILD_DIR) ./meson_init.sh -f -t "$(SHODAN_BOOT_ROM_BUILD_TOOLCHAIN_CONFIG)";
 
-$(SHODAN_MANIFEST): $(SHODAN_BUILD_NINJA_SCRIPT)
+$(SHODAN_BOOT_ROM_MANIFEST): $(SHODAN_BOOT_ROM_BUILD_NINJA_SCRIPT)
 	cd $(ROOTDIR)/hw/opentitan; \
-		ninja -C $(SHODAN_BUILD_OUT_DIR) \
+		ninja -C $(SHODAN_BOOT_ROM_BUILD_OUT_DIR) \
 			sw_sparrow/device/rom_exts/manifest.h \
 			sw/device/rom_exts/manifest.h
 
-multihart_boot_rom: $(SHODAN_DEPS)
+multihart_boot_rom: $(SHODAN_BOOT_ROM_DEPS)
 	cd $(ROOTDIR)/sw/multihart_boot_rom; \
-		BUILD_ROOT=$(SHODAN_BOOT_ROM_BUILD_DIR) ./meson_init.sh -f -t "$(SHODAN_BUILD_TOOLCHAIN_CONFIG)"; \
-		ninja -C $(SHODAN_BUILD_OUT_DIR) \
+		BUILD_ROOT=$(SHODAN_BOOT_ROM_BUILD_DIR) ./meson_init.sh -f -t "$(SHODAN_BOOT_ROM_BUILD_TOOLCHAIN_CONFIG)"; \
+		ninja -C $(SHODAN_BOOT_ROM_BUILD_OUT_DIR) \
 			multihart_boot_rom/multihart_boot_rom_export_sim_verilator;
 
 multihart_boot_rom_clean:
