3443ff05482278eed382f8d4382d73727afecac6
Cindy Liu <hcindyl@google.com>
2021-04-12 17:35:45 -0700

    Cleanup sim.mk
    * Edit the phony targets renode and verilator to the final build artifact
    * Split mkdir to another order-only target
    * Cleanup the necessary pushd
    * Change *_clean to remove the build and install directories
    
    Note: to force rebuild the target, remove the *_BIN file to trigger
    rebuild.
    
    Change-Id: I6d2e9b356c1972acc4e4301e8d52effb16eeade0
    GitOrigin-RevId: 232757c1847598e826e242ec004234f7fa24a285

--- sim.mk
+++ sim.mk
@@ -1,17 +1,19 @@
 RENODE_SRC_DIR := $(ROOTDIR)/sim/renode
 RENODE_OUT_DIR := $(OUT)/host/renode
+RENODE_BIN     := $(RENODE_OUT_DIR)/Renode.exe
 
+$(RENODE_OUT_DIR):
+	mkdir -p $(RENODE_OUT_DIR)
 
-$(RENODE_OUT_DIR): | $(RENODE_SRC_DIR)
-	pushd $(ROOTDIR); mkdir -p $(RENODE_OUT_DIR);
-	pushd $(RENODE_SRC_DIR); \
+$(RENODE_BIN): | $(RENODE_SRC_DIR) $(RENODE_OUT_DIR)
+	pushd $(RENODE_SRC_DIR) > /dev/null; \
 	    ./build.sh -S; \
 	    cp -rf output/bin/Release/* $(RENODE_OUT_DIR); \
 	    cp -rf scripts $(RENODE_OUT_DIR); \
 	    cp -rf platforms $(RENODE_OUT_DIR); \
-	popd
 
-renode: $(RENODE_OUT_DIR)
+# To rebuild the phony target, romove $(RENODE_BIN) to trigger the rebuild.
+renode: $(RENODE_BIN)
 
 renode_clean:
 	@rm -rf $(RENODE_OUT_DIR)
@@ -19,19 +21,23 @@ renode_clean:
 VERILATOR_SRC_DIR   := $(ROOTDIR)/sim/verilator
 VERILATOR_BUILD_DIR := $(OUT)/tmp/verilator
 VERILATOR_OUT_DIR   := $(OUT)/host/verilator
+VERILATOR_BIN       := $(VERILATOR_OUT_DIR)/bin/verilator_bin
 
-$(VERILATOR_OUT_DIR): | $(ROOTDIR)/sim/verilator
-	pushd $(ROOTDIR); mkdir -p $(VERILATOR_BUILD_DIR);
-	pushd $(VERILATOR_BUILD_DIR); autoconf -o $(VERILATOR_BUILD_DIR)/configure $(VERILATOR_SRC_DIR)/configure.ac
-	pushd $(VERILATOR_BUILD_DIR); sh configure \
+$(VERILATOR_BUILD_DIR):
+	mkdir -p $(VERILATOR_BUILD_DIR)
+
+$(VERILATOR_BIN): | $(VERILATOR_SRC_DIR) $(VERILATOR_BUILD_DIR)
+	autoconf -o $(VERILATOR_BUILD_DIR)/configure $(VERILATOR_SRC_DIR)/configure.ac
+	pushd $(VERILATOR_BUILD_DIR) > /dev/null; sh configure \
 		--srcdir=$(VERILATOR_SRC_DIR) \
 		--prefix=$(VERILATOR_OUT_DIR)
-	pushd $(VERILATOR_BUILD_DIR); make -j$(shell nproc)
-	pushd $(VERILATOR_BUILD_DIR); make install
+	make -j$(shell nproc) -C $(VERILATOR_BUILD_DIR)
+	make -C $(VERILATOR_BUILD_DIR) install
 
 verilator_clean:
-	pushd $(VERILATOR_BUILD_DIR); make clean;
+	rm -rf $(VERILATOR_BUILD_DIR) $(VERILATOR_OUT_DIR)
 
-verilator: $(VERILATOR_OUT_DIR)
+# To rebuild the phony target, romove $(VERILATOR_BIN) to trigger the rebuild.
+verilator: $(VERILATOR_BIN)
 
 .PHONY:: renode verilator
