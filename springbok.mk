b83ebea48460f8588543fcd340d2298062337ac1
Henry Herman <henryherman@google.com>
2021-06-17 22:05:37 +0000

    Merge "Use cmake to build springbok targets."
    GitOrigin-RevId: 0daac7b4a117bb7b61b36d985ebbed96d8a1dec0

--- springbok.mk
+++ springbok.mk
@@ -2,8 +2,8 @@ SPRINGBOK_BUILD_DIR=$(OUT)/springbok/
 SPRINGBOK_SRC_DIR=$(ROOTDIR)/sw/vec
 
 springbok:
-	cd $(SPRINGBOK_SRC_DIR)/hello_vec; \
-		make OUTPREFIX=$(SPRINGBOK_BUILD_DIR)
+	cmake -B $(SPRINGBOK_BUILD_DIR) -GNinja $(SPRINGBOK_SRC_DIR)
+	cmake --build $(SPRINGBOK_BUILD_DIR)
 
 clean_springbok:
 	@rm -rf $(SPRINGBOK_BUILD_DIR)
