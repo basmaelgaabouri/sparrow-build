12c951ace6d879207c2ee33071d87a0c31d6fcd7
Henry Herman <henryherman@google.com>
2021-07-02 21:25:07 +0000

    Merge "renode: Build renode with debug symbols."
    GitOrigin-RevId: 744a88fd9c6e7eeb2c5e1f4d053a050b35396237

--- sim.mk
+++ sim.mk
@@ -9,7 +9,7 @@ $(RENODE_OUT_DIR):
 
 $(RENODE_BIN): | $(RENODE_SRC_DIR) $(RENODE_OUT_DIR)
 	pushd $(RENODE_SRC_DIR) > /dev/null; \
-	    ./build.sh --skip-fetch; \
+	    ./build.sh -d --skip-fetch; \
 	    cp -rf output/bin/Release/* $(RENODE_OUT_DIR); \
 	    cp -rf scripts $(RENODE_OUT_DIR); \
 	    cp -rf platforms $(RENODE_OUT_DIR)
