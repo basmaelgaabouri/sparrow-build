44edd56e1e7a2e82e14e3e6606048b34050b7ec4
Matt Harvey <mattharvey@google.com>
2021-07-12 11:26:54 -0700

    Adds kata Makefile step call OpenTitan regtool
    To write s3L4 for OpenTitan matcha, we want to depend on generated C
    header files for definitions of MMIO sizes, offsets, masks, bit
    meanings, etc.
    
    To make these header files available to the seL4 build, this change adds
    a make prerequisite for the kata build to run regtool.py with output
    going to out/kata/opentitan-gen.
    
    In CAmkES CMake files, the INCLUDE attribute can be used to add to the
    include path, and this change also does that for the UartDriver
    component. (The implementation will be left for a later change, since an
    initial straighforwrd attempt saw writes still going to the 16550.)
    
    Change-Id: I4b1f6b5ed7f77e9218e1f8b249550d5ec387f912
    GitOrigin-RevId: 9c2792fe0db3906225e2b9e83cb07408cfd7d383

--- kata.mk
+++ kata.mk
@@ -5,9 +5,21 @@ KATA_SOURCES := $(shell find $(ROOTDIR)/kata \
 						-name \*.cpp \
 						-type f)
 
-kata: $(KATA_SOURCES)
+OPENTITAN_GEN=$(OUT)/kata/opentitan-gen/include/opentitan
+
+$(OPENTITAN_GEN):
+	mkdir -p $(OPENTITAN_GEN)
+	cd $(ROOTDIR)/hw/opentitan; \
+		python3 util/regtool.py -D -o $(OPENTITAN_GEN)/uart.h hw/ip/uart/data/uart.hjson
+
+kata-clean-opentitan-headers:
+	rm -rf $(OPENTITAN_GEN)
+
+kata-opentitan-headers: kata-clean-opentitan-headers $(OPENTITAN_GEN)
+
+kata: $(KATA_SOURCES) $(OPENTITAN_GEN)
 	mkdir -p $(OUT)/kata
 	cd $(OUT)/kata && cmake -G Ninja -DCROSS_COMPILER_PREFIX=riscv32-unknown-elf- -DSIMULATION=0 $(ROOTDIR)/kata/projects/processmanager
 	cd $(OUT)/kata && ninja
 
-.PHONY:: kata
+.PHONY:: kata kata-opentitan-headers
