09e02c8799425d377b7d077a56b4dab2a5bddf05
Henry Herman <henryherman@google.com>
2021-07-16 19:00:59 +0000

    Add target for running springbok tests.
    Change-Id: I106149ae5069a4d5d19e98cee4b39a1fa8b661ab
    GitOrigin-RevId: 5fff19c75a1cc609a16030426d77dd93f8bedd91

--- springbok.mk
+++ springbok.mk
@@ -10,8 +10,12 @@ springbok_iree:
 	cmake -B $(SPRINGBOK_IREE_BUILD_DIR) -GNinja $(SPRINGBOK_SRC_DIR) -DBUILD_IREE=ON
 	cmake --build $(SPRINGBOK_IREE_BUILD_DIR) --target springbok_intrinsic
 
+test_springbok:
+	cmake -B $(SPRINGBOK_BUILD_DIR) -GNinja $(SPRINGBOK_SRC_DIR)
+	cmake --build $(SPRINGBOK_BUILD_DIR) --target AllTests
+
 clean_springbok:
 	@rm -rf $(SPRINGBOK_BUILD_DIR)
 	@rm -rf $(SPRINGBOK_IREE_BUILD_DIR)
 
-.PHONY:: springbok clean_springbok
+.PHONY:: springbok clean_springbok test_springbok springbok_iree
