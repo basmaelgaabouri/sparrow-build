fecce897b9052b782b2dc90ef17316efdb0976eb
Adam Jesionowski <jesionowski@google.com>
2021-07-20 15:18:08 -0700

    Add springbok_iree target and link with new lib.
    The springbok_iree target builds the libspringbok_intrinsic with the
    IREE rv32 toolchain. Switch the IREE riscv settings to use this library
    instead.
    
    Change-Id: I6ba303cfa229395876251554e8e9fce1ba4a4a2c
    GitOrigin-RevId: e31bcd7387f40610fa5009f9442ff6de38923159

--- iree.mk
+++ iree.mk
@@ -1,18 +1,14 @@
-IREE_TOOLCHAIN=$(CACHE)/toolchain_iree
 IREE_SRC=$(ROOTDIR)/toolchain/iree
 IREE_HOST_OUT=$(OUT)/host/iree-build-host
-IREE_RISCV_OUT=$(OUT)/host/iree-build-riscv64
 IREE_RISCV32_OUT=$(OUT)/host/iree-build-riscv32
-QEMU_PATH=${OUT}/host/qemu/riscv64-linux-user
+QEMU_PATH=$(OUT)/host/qemu/riscv32-softmmu
 TOOLCHAINRV32_PATH=$(CACHE)/toolchain_iree_rv32imf
-LINKER_SCRIPT=$(ROOTDIR)/sw/vec/springbok/matcha.ld
-CRT0=$(ROOTDIR)/sw/vec/springbok/crt0.s
 
 # The following targets are always rebuilt when the iree target is made
 
 iree_check:
-	if [[ ! -d "$(IREE_TOOLCHAIN)" ]]; then \
-		echo "IREE toolchain $(IREE_TOOLCHAIN) doesn't exist, please run 'm install_llvm' first"; \
+	if [[ ! -d "$(TOOLCHAINRV32_PATH)" ]]; then \
+		echo "IREE toolchain $(TOOLCHAINRV32_PATH) doesn't exist, please run 'm install_llvm' first"; \
 		exit 1; \
 	fi
 	if [ ! -d "${QEMU_PATH}" ]; then \
@@ -28,17 +24,7 @@ iree_host_build: | iree_check
 	    $(IREE_SRC)
 	cmake --build $(IREE_HOST_OUT) --target install
 
-iree_riscv_build: | iree_check
-	cmake -G Ninja -B $(IREE_RISCV_OUT) \
-	    -DCMAKE_TOOLCHAIN_FILE="$(ROOTDIR)/build/riscv_iree.cmake" \
-	    -DIREE_HOST_BINARY_ROOT="$(IREE_HOST_OUT)/install" \
-	    -DRISCV_CPU=rv64 -DIREE_BUILD_COMPILER=OFF \
-	    -DIREE_ENABLE_MLIR=OFF -DIREE_BUILD_SAMPLES=OFF \
-	    -DRISCV_TOOLCHAIN_ROOT=$(IREE_TOOLCHAIN) \
-	    $(IREE_SRC)
-	cmake --build $(IREE_RISCV_OUT)
-
-iree_rv32_build:
+iree_rv32_build: | springbok_iree
 	cmake -G Ninja -B $(IREE_RISCV32_OUT) \
 	    -DCMAKE_TOOLCHAIN_FILE="$(ROOTDIR)/build/riscv_iree.cmake" \
 	    -DIREE_HOST_BINARY_ROOT="$(IREE_HOST_OUT)/install" \
@@ -51,12 +37,9 @@ iree_rv32_build:
 # TODO: Build everything, not just target. Hits 256k limitation on some examples.
 	cmake --build $(IREE_RISCV32_OUT) --target iree/hal/local/elf/elf_module_test_binary
 
-iree_test:
-	bash ${ROOTDIR}/scripts/run-iree.sh
-
-iree: iree_host_build iree_riscv_build iree_test
+iree: iree_host_build iree_rv32_build
 
 iree_clean:
-	rm -rf $(IREE_HOST_OUT) $(IREE_RISCV_OUT) $(IREE_RISCV32_OUT)
+	rm -rf $(IREE_HOST_OUT) $(IREE_RISCV32_OUT)
 
-.PHONY:: iree iree_check iree_host_build iree_riscv_build iree_test iree_clean
+.PHONY:: iree iree_check iree_host_build iree_rv32_build iree_clean
--- springbok.mk
+++ springbok.mk
@@ -1,11 +1,17 @@
 SPRINGBOK_BUILD_DIR=$(OUT)/springbok/
+SPRINGBOK_IREE_BUILD_DIR=$(OUT)/springbok_iree/
 SPRINGBOK_SRC_DIR=$(ROOTDIR)/sw/vec
 
 springbok:
 	cmake -B $(SPRINGBOK_BUILD_DIR) -GNinja $(SPRINGBOK_SRC_DIR)
 	cmake --build $(SPRINGBOK_BUILD_DIR)
 
+springbok_iree:
+	cmake -B $(SPRINGBOK_IREE_BUILD_DIR) -GNinja $(SPRINGBOK_SRC_DIR) -DBUILD_IREE=ON
+	cmake --build $(SPRINGBOK_IREE_BUILD_DIR) --target springbok_intrinsic
+
 clean_springbok:
 	@rm -rf $(SPRINGBOK_BUILD_DIR)
+	@rm -rf $(SPRINGBOK_IREE_BUILD_DIR)
 
 .PHONY:: springbok clean_springbok
