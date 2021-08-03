d23ad2cf0ebdbcf2bdd305e722695c3d551f760d
Matt Harvey <mattharvey@google.com>
2021-08-03 10:41:15 -0700

    Adds PLIC register defines to regtool make target
    Change-Id: Ifc508f1b78f8abd2b44e743072b709d32c6d14d1
    GitOrigin-RevId: 1df2a08876923cee0378d3035ad87c0385d0adfb

--- kata.mk
+++ kata.mk
@@ -6,11 +6,16 @@ KATA_SOURCES := $(shell find $(ROOTDIR)/kata \
 						-type f)
 
 OPENTITAN_GEN=$(OUT)/kata/opentitan-gen/include/opentitan
+PLIC_HJSON=$(OPENTITAN_GEN)/rv_plic.hjson
 
 $(OPENTITAN_GEN):
 	mkdir -p $(OPENTITAN_GEN)
+	# TODO(mattharvey): Migrate UART to opentitan-upstream.
 	cd $(ROOTDIR)/hw/opentitan; \
-		python3 util/regtool.py -D -o $(OPENTITAN_GEN)/uart.h hw/ip/uart/data/uart.hjson
+		python3 util/regtool.py -D -o $(OPENTITAN_GEN)/uart.h hw/ip/uart/data/uart.hjson; \
+	cd $(ROOTDIR)/hw/opentitan-upstream; \
+		python3 hw/ip/rv_plic/util/reg_rv_plic.py -s 96 -t 2 -p 7 hw/ip/rv_plic/data/rv_plic.hjson.tpl > $(PLIC_HJSON); \
+		python3 util/regtool.py -D -o $(OPENTITAN_GEN)/plic.h $(PLIC_HJSON)
 
 kata-clean-opentitan-headers:
 	rm -rf $(OPENTITAN_GEN)
