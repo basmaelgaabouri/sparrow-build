b99388955d31f8fa36dc32fd06938bde88803796
Matt Harvey <mattharvey@google.com>
2021-08-20 12:04:14 -0700

    Make target for timer.h (Kata on OpenTitan)
    This generates register definitions assuming two timers (-t 2) for each
    of two harts (-s 2).
    
    It also includes some Makefile readability cleanup for all of the
    related register definition headers.
    
    Change-Id: If6ea24c4bf628816af9f920eef1d68f5ac962ee7
    GitOrigin-RevId: 197b5774d496b754f4ee3684085d87f6097ae388

--- kata.mk
+++ kata.mk
@@ -5,24 +5,46 @@ KATA_SOURCES := $(shell find $(ROOTDIR)/kata \
 						-name \*.cpp \
 						-type f)
 
+OPENTITAN_SOURCE=$(ROOTDIR)/hw/opentitan-upstream
 OPENTITAN_GEN=$(OUT)/kata/opentitan-gen/include/opentitan
-PLIC_HJSON=$(OPENTITAN_GEN)/rv_plic.hjson
+REGTOOL=$(OPENTITAN_SOURCE)/util/regtool.py
 
 $(OPENTITAN_GEN):
 	mkdir -p $(OPENTITAN_GEN)
-	# TODO(mattharvey): Migrate UART to opentitan-upstream.
-	cd $(ROOTDIR)/hw/opentitan; \
-		python3 util/regtool.py -D -o $(OPENTITAN_GEN)/uart.h hw/ip/uart/data/uart.hjson; \
-	cd $(ROOTDIR)/hw/opentitan-upstream; \
-		python3 hw/ip/rv_plic/util/reg_rv_plic.py -s 96 -t 2 -p 7 hw/ip/rv_plic/data/rv_plic.hjson.tpl > $(PLIC_HJSON); \
-		python3 util/regtool.py -D -o $(OPENTITAN_GEN)/plic.h $(PLIC_HJSON)
+
+PLIC_HEADER=$(OPENTITAN_GEN)/plic.h
+$(PLIC_HEADER): IP_DIR=$(OPENTITAN_SOURCE)/hw/ip/rv_plic
+$(PLIC_HEADER): JINJA=$(IP_DIR)/util/reg_rv_plic.py
+$(PLIC_HEADER): TEMPLATE=$(IP_DIR)/data/rv_plic.hjson.tpl
+$(PLIC_HEADER): HJSON=$(OPENTITAN_GEN)/rv_plic.hjson
+$(PLIC_HEADER): $(OPENTITAN_GEN) $(JINJA) $(TEMPLATE) $(REGTOOL)
+	$(JINJA) -s 96 -t 2 -p 7 $(TEMPLATE) > $(HJSON)
+	$(REGTOOL) -D -o $(PLIC_HEADER) $(HJSON)
+
+TIMER_HEADER=$(OPENTITAN_GEN)/timer.h
+$(TIMER_HEADER): IP_DIR=$(OPENTITAN_SOURCE)/hw/ip/rv_timer
+$(TIMER_HEADER): JINJA=$(IP_DIR)/util/reg_timer.py
+$(TIMER_HEADER): TEMPLATE=$(IP_DIR)/data/rv_timer.hjson.tpl
+$(TIMER_HEADER): HJSON=$(OPENTITAN_GEN)/rv_timer.hjson
+$(TIMER_HEADER): $(OPENTITAN_GEN) $(JINJA) $(TEMPLATE) $(REGTOOL)
+	$(JINJA) -s 2 -t 2 $(TEMPLATE) > $(HJSON)
+	$(REGTOOL) -D -o $(TIMER_HEADER) $(HJSON)
+
+UART_HEADER=$(OPENTITAN_GEN)/uart.h
+# TODO(mattharvey): Migrate UART to opentitan-upstream.
+$(UART_HEADER): OPENTITAN_SOURCE=$(ROOTDIR)/hw/opentitan
+$(UART_HEADER): REGTOOL=$(OPENTITAN_SOURCE)/util/regtool.py
+$(UART_HEADER): IP_DIR=$(ROOTDIR)/hw/opentitan/hw/ip/uart
+$(UART_HEADER): HJSON=$(IP_DIR)/data/uart.hjson
+$(UART_HEADER): $(OPENTITAN_GEN) $(TEMPLATE) $(REGTOOL)
+	$(REGTOOL) -D -o $(UART_HEADER) $(HJSON)
 
 kata-clean-opentitan-headers:
 	rm -rf $(OPENTITAN_GEN)
 
-kata-opentitan-headers: kata-clean-opentitan-headers $(OPENTITAN_GEN)
+kata-opentitan-headers: $(PLIC_HEADER) $(TIMER_HEADER) $(UART_HEADER)
 
-kata: $(KATA_SOURCES) $(OPENTITAN_GEN)
+kata: $(KATA_SOURCES) kata-opentitan-headers
 	mkdir -p $(OUT)/kata
 	cd $(OUT)/kata && cmake -G Ninja \
 		-DCROSS_COMPILER_PREFIX=riscv32-unknown-elf- \
@@ -31,4 +53,4 @@ kata: $(KATA_SOURCES) $(OPENTITAN_GEN)
 		$(ROOTDIR)/kata/projects/processmanager
 	cd $(OUT)/kata && ninja
 
-.PHONY:: kata kata-opentitan-headers
+.PHONY:: kata kata-clean-opentitan-headers
