bd98a8566c2dfa00f82d988dd87b5b8158633fa4
Matt Harvey <mattharvey@google.com>
2021-08-23 15:35:32 -0700

    Direct reference to $(RENODE_BIN) in simulate
    The script sim/renode/renode contains some logic around the directory
    name that can lead to not running the intended build. This change
    updates our Makefile targets for running simulations to use the same
    make variable that refers to the Renode.exe in the build output.
    
    Change-Id: If6ae88fe766e2f4965e0d975759c9ad3911953b9
    GitOrigin-RevId: debcbd0fbf09c3729a55ee055650c12ef72d0085

--- sim.mk
+++ sim.mk
@@ -1,6 +1,7 @@
 RENODE_SRC_DIR := $(ROOTDIR)/sim/renode
 RENODE_OUT_DIR := $(OUT)/host/renode
 RENODE_BIN     := $(RENODE_OUT_DIR)/Renode.exe
+RENODE_CMD     := cd $(ROOTDIR) && mono $(RENODE_BIN) --disable-xwt
 
 RENODE_SIM_GENERATOR_SCRIPT := $(ROOTDIR)/scripts/generate_renode_configs.sh
 
@@ -8,13 +9,13 @@ $(RENODE_OUT_DIR):
 	mkdir -p $(RENODE_OUT_DIR)
 
 $(RENODE_BIN): | $(RENODE_SRC_DIR) $(RENODE_OUT_DIR)
-	pushd $(RENODE_SRC_DIR) > /dev/null; \
+	cd $(RENODE_SRC_DIR) > /dev/null; \
 		./build.sh -d --skip-fetch; \
 		cp -rf output/bin/Debug/* $(RENODE_OUT_DIR); \
 		cp -rf scripts $(RENODE_OUT_DIR); \
 		cp -rf platforms $(RENODE_OUT_DIR)
 
-# To rebuild the phony target, romove $(RENODE_BIN) to trigger the rebuild.
+# To rebuild the phony target, remove $(RENODE_BIN) to trigger the rebuild.
 renode: $(RENODE_BIN)
 
 renode_clean:
@@ -29,9 +30,9 @@ $(VERILATOR_BUILD_DIR):
 	mkdir -p $(VERILATOR_BUILD_DIR)
 
 $(VERILATOR_BIN): | $(VERILATOR_SRC_DIR) $(VERILATOR_BUILD_DIR)
-	pushd $(VERILATOR_BUILD_DIR) > /dev/null; \
+	cd $(VERILATOR_BUILD_DIR) > /dev/null; \
 		autoconf -o $(VERILATOR_BUILD_DIR)/configure $(VERILATOR_SRC_DIR)/configure.ac
-	pushd $(VERILATOR_BUILD_DIR) > /dev/null; sh configure \
+	cd $(VERILATOR_BUILD_DIR) > /dev/null; sh configure \
 		--srcdir=$(VERILATOR_SRC_DIR) \
 		--prefix=$(VERILATOR_OUT_DIR)
 	make -j$(shell nproc) -C $(VERILATOR_BUILD_DIR)
@@ -68,18 +69,18 @@ $(OUT)/ext_flash.tar: tock kata
 sim_deps: renode multihart_boot_rom $(OUT)/ext_flash.tar springbok
 
 simulate: sim_deps
-	$(ROOTDIR)/sim/renode/renode -e "i @sim/config/sparrow_all.resc; pause; cpu0 IsHalted false; cpu1 IsHalted false; start" --disable-xwt
+	$(RENODE_CMD) -e "i @sim/config/sparrow_all.resc; pause; cpu0 IsHalted false; cpu1 IsHalted false; start"
 
 debug-simulation: sim_deps
-	$(ROOTDIR)/sim/renode/renode -e "i @sim/config/sparrow_all.resc; start" --disable-xwt
+	$(RENODE_CMD) -e "i @sim/config/sparrow_all.resc; start"
 
 test_sc: renode multihart_boot_rom $(ROOTDIR)/sim/config/sparrow_all.resc
-	$(ROOTDIR)/sim/renode/renode -e "\$$tar = @$(ROOTDIR)/out/test_sc.tar; i @sim/config/sparrow_all.resc; pause; cpu0 IsHalted false; cpu1 IsHalted false; start" --disable-xwt
+	$(RENODE_CMD) -e "\$$tar = @$(ROOTDIR)/out/test_sc.tar; i @sim/config/sparrow_all.resc; pause; cpu0 IsHalted false; cpu1 IsHalted false; start"
 
 test_mc: renode multihart_boot_rom $(ROOTDIR)/sim/config/sparrow_all.resc
-	$(ROOTDIR)/sim/renode/renode -e "\$$tar = @$(ROOTDIR)/out/test_mc.tar; i @sim/config/sparrow_all.resc; pause; cpu0 IsHalted false; cpu1 IsHalted false; start" --disable-xwt
+	$(RENODE_CMD) -e "\$$tar = @$(ROOTDIR)/out/test_mc.tar; i @sim/config/sparrow_all.resc; pause; cpu0 IsHalted false; cpu1 IsHalted false; start"
 
-test_vc: renode multihart_boot_rom $(ROOTDIR)/sim/config/sparrow_all.resc
-	$(ROOTDIR)/sim/renode/renode -e "\$$tar = @$(ROOTDIR)/out/test_vc.tar; i @sim/config/sparrow_all.resc; pause; cpu0 IsHalted false; cpu1 IsHalted false; start" --disable-xwt
+tereturnst_vc: renode multihart_boot_rom $(ROOTDIR)/sim/config/sparrow_all.resc
+	$(RENODE_CMD) -e "\$$tar = @$(ROOTDIR)/out/test_vc.tar; i @sim/config/sparrow_all.resc; pause; cpu0 IsHalted false; cpu1 IsHalted false; start"
 
 .PHONY:: renode verilator sim_configs clean_sim_configs
