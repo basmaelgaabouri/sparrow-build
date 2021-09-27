bc43f7cb72a2ea34f604eec27f941494ad1d0a68
June Tate-Gans <jtgans@google.com>
2021-09-27 16:04:30 +0000

    Merge "build: Ensure HJSON files depend on the HJSON"
    GitOrigin-RevId: c4556a819e1c0eb6a59b8f771288f8d47184984d

--- kata.mk
+++ kata.mk
@@ -14,39 +14,39 @@ $(OPENTITAN_GEN):
 	mkdir -p $(OPENTITAN_GEN)
 
 PLIC_HEADER=$(OPENTITAN_GEN)/plic.h
+PLIC_HJSON=$(OPENTITAN_GEN)/rv_plic.hjson
 $(PLIC_HEADER): IP_DIR=$(OPENTITAN_SOURCE)/hw/ip/rv_plic
 $(PLIC_HEADER): JINJA=$(IP_DIR)/util/reg_rv_plic.py
 $(PLIC_HEADER): TEMPLATE=$(IP_DIR)/data/rv_plic.hjson.tpl
-$(PLIC_HEADER): HJSON=$(OPENTITAN_GEN)/rv_plic.hjson
-$(PLIC_HEADER): $(OPENTITAN_GEN) $(JINJA) $(TEMPLATE) $(REGTOOL)
-	$(JINJA) -s 96 -t 2 -p 7 $(TEMPLATE) > $(HJSON)
-	$(REGTOOL) -D -o $(PLIC_HEADER) $(HJSON)
+$(PLIC_HEADER): $(OPENTITAN_GEN) $(JINJA) $(TEMPLATE) $(REGTOOL) $(PLIC_HJSON)
+	$(JINJA) -s 96 -t 2 -p 7 $(TEMPLATE) > $(PLIC_HJSON)
+	$(REGTOOL) -D -o $(PLIC_HEADER) $(PLIC_HJSON)
 
 TIMER_HEADER=$(OPENTITAN_GEN)/timer.h
+TIMER_HJSON=$(OPENTITAN_GEN)/rv_timer.hjson
 $(TIMER_HEADER): IP_DIR=$(OPENTITAN_SOURCE)/hw/ip/rv_timer
 $(TIMER_HEADER): JINJA=$(IP_DIR)/util/reg_timer.py
 $(TIMER_HEADER): TEMPLATE=$(IP_DIR)/data/rv_timer.hjson.tpl
-$(TIMER_HEADER): HJSON=$(OPENTITAN_GEN)/rv_timer.hjson
-$(TIMER_HEADER): $(OPENTITAN_GEN) $(JINJA) $(TEMPLATE) $(REGTOOL)
-	$(JINJA) -s 2 -t 1 $(TEMPLATE) > $(HJSON)
-	$(REGTOOL) -D -o $(TIMER_HEADER) $(HJSON)
+$(TIMER_HEADER): $(OPENTITAN_GEN) $(JINJA) $(TEMPLATE) $(REGTOOL) $(TIMER_HJSON)
+	$(JINJA) -s 2 -t 1 $(TEMPLATE) > $(TIMER_HJSON)
+	$(REGTOOL) -D -o $(TIMER_HEADER) $(TIMER_HJSON)
 
 UART_HEADER=$(OPENTITAN_GEN)/uart.h
+UART_HJSON=$(IP_DIR)/data/uart.hjson
 # TODO(mattharvey): Migrate UART to opentitan-upstream.
 $(UART_HEADER): OPENTITAN_SOURCE=$(ROOTDIR)/hw/opentitan
 $(UART_HEADER): REGTOOL=$(OPENTITAN_SOURCE)/util/regtool.py
 $(UART_HEADER): IP_DIR=$(ROOTDIR)/hw/opentitan/hw/ip/uart
-$(UART_HEADER): HJSON=$(IP_DIR)/data/uart.hjson
-$(UART_HEADER): $(OPENTITAN_GEN) $(TEMPLATE) $(REGTOOL)
-	$(REGTOOL) -D -o $(UART_HEADER) $(HJSON)
+$(UART_HEADER): $(OPENTITAN_GEN) $(REGTOOL) $(UART_HJSON)
+	$(REGTOOL) -D -o $(UART_HEADER) $(UART_HJSON)
 
 $(VC_TOP_GEN):
 	mkdir -p $(VC_TOP_GEN)
 
 VC_TOP_HEADER=$(VC_TOP_GEN)/vc_top.h
-$(VC_TOP_HEADER): HJSON=$(OPENTITAN_SOURCE)/hw/ip/vc_top/data/vc_top.hjson
-$(VC_TOP_HEADER): $(VC_TOP_GEN) $(TEMPLATE) $(REGTOOL)
-	$(REGTOOL) -D -o $(VC_TOP_HEADER) $(HJSON)
+VC_TOP_HJSON=$(OPENTITAN_SOURCE)/hw/ip/vc_top/data/vc_top.hjson
+$(VC_TOP_HEADER): $(VC_TOP_GEN) $(REGTOOL) $(VC_TOP_HJSON)
+	$(REGTOOL) -D -o $(VC_TOP_HEADER) $(VC_TOP_HJSON)
 
 kata-clean-headers:
 	rm -rf $(OPENTITAN_GEN) $(VC_TOP_HEADER)
@@ -62,4 +62,4 @@ kata: $(KATA_SOURCES) kata-gen-headers
 		$(ROOTDIR)/kata/projects/processmanager
 	cd $(OUT)/kata && ninja
 
-.PHONY:: kata kata-clean-headers
+.PHONY:: kata kata-clean-headers kata-gen-headers
