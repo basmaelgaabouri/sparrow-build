619c6db184e08d81dac53513c8e73abfc18ca728
Lun Dong <lundong@google.com>
2021-10-15 19:37:31 -0700

    build:iree: Unpin IREE host compiler to a specific snapshot
    To revert the workaround as the upstream breakage has been fixed
    (verified).
    
    Bug: 202859345
    Change-Id: I071cf688ee3b9e7fb76e64df4ef353e928cedb8e
    GitOrigin-RevId: 406e0bc4c7084fecda902a103d05470f8194fbf4

--- iree.mk
+++ iree.mk
@@ -44,9 +44,8 @@ $(IREE_COMPILER_DIR):
 # the `iree_runtime` target. The outputs of this target are placed in
 # out/host/iree_compiler.
 #
-# TODO(b/202859345): Un-pin the snapshot release
 iree_compiler: | $(IREE_COMPILER_DIR)
-	scripts/download_iree_compiler.py --tag snapshot-20211011.579
+	scripts/download_iree_compiler.py
 
 iree_commit_check:
 	scripts/check-iree-commit.sh $(IREE_SRC)
