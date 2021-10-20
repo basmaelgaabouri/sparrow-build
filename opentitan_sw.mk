a739aa397e0248862613ffb0177b50e55563b290
Julian Mullings-Black <julianmb@google.com>
2021-10-20 17:56:34 +0000

    Merge "Switch OT make targets to use newer OT snapshot"
    GitOrigin-RevId: fd03873d1b8bd3bb8a09daa55d09d3dce7e204b1

--- opentitan_sw.mk
+++ opentitan_sw.mk
@@ -1,24 +1,28 @@
 
+OPENTITAN_SRC_DIR=$(ROOTDIR)/hw/opentitan-upstream
 OPENTITAN_BUILD_DIR=$(OUT)/opentitan
 OPENTITAN_BUILD_OUT_DIR=$(OPENTITAN_BUILD_DIR)/build-out
 
 $(OPENTITAN_BUILD_OUT_DIR):
 	@echo "Creating output directory $(OPENTITAN_BUILD_DIR)"
 	@mkdir -p "$(OPENTITAN_BUILD_DIR)"
-	cd $(ROOTDIR)/hw/opentitan; \
-	    TOOLCHAIN_PATH=$(CACHE)/toolchain BUILD_ROOT=$(OPENTITAN_BUILD_DIR) ./meson_init.sh -f;
+	cd $(OPENTITAN_SRC_DIR); \
+		TOOLCHAIN_PATH=$(CACHE)/toolchain \
+		BUILD_ROOT=$(OPENTITAN_BUILD_DIR) \
+		CC_FOR_BUILD=gcc \
+		CXX_FOR_BUILD=g++ ./meson_init.sh -f;
 
-## Builds the hardware testing binaries from OpenTitan in hw/opentitan
+## Builds the hardware testing binaries from OpenTitan in hw/opentitan-upstream
 opentitan_sw_all: $(OPENTITAN_BUILD_OUT_DIR)
-	cd $(ROOTDIR)/hw/opentitan; \
+	cd $(OPENTITAN_SRC_DIR); \
 	    ninja -C $(OPENTITAN_BUILD_OUT_DIR) all
 
 opentitan_sw_helloworld: $(OPENTITAN_BUILD_OUT_DIR)
-	cd $(ROOTDIR)/hw/opentitan; \
+	cd $(OPENTITAN_SRC_DIR); \
 	    ninja -C $(OPENTITAN_BUILD_OUT_DIR) sw/device/examples/hello_world/hello_world_export_sim_verilator;
 
 opentitan_sw_bootrom: $(OPENTITAN_BUILD_OUT_DIR)
-	cd $(ROOTDIR)/hw/opentitan; \
+	cd $(OPENTITAN_SRC_DIR); \
 	ninja -C $(OPENTITAN_BUILD_OUT_DIR) \
 		sw/device/boot_rom/boot_rom_export_sim_verilator \
 		sw/device/boot_rom/boot_rom_export_sim_dv \
