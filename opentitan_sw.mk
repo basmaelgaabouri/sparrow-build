26333fe7c9a21c0a44e5b0e6767fa29cfecf50cb
Julian Mullings-Black <julianmb@google.com>
2021-10-21 18:33:06 +0000

    Merge "Add a make target to invoke opentitan host tests"
    GitOrigin-RevId: 5d2c4b32f7660eed9fb139d77abe1ffd5a090421

--- opentitan_sw.mk
+++ opentitan_sw.mk
@@ -14,22 +14,23 @@ $(OPENTITAN_BUILD_OUT_DIR):
 
 ## Builds the hardware testing binaries from OpenTitan in hw/opentitan-upstream
 opentitan_sw_all: $(OPENTITAN_BUILD_OUT_DIR)
-	cd $(OPENTITAN_SRC_DIR); \
-	    ninja -C $(OPENTITAN_BUILD_OUT_DIR) all
+	ninja -C $(OPENTITAN_BUILD_OUT_DIR) all
 
 opentitan_sw_helloworld: $(OPENTITAN_BUILD_OUT_DIR)
-	cd $(OPENTITAN_SRC_DIR); \
-	    ninja -C $(OPENTITAN_BUILD_OUT_DIR) sw/device/examples/hello_world/hello_world_export_sim_verilator;
+	ninja -C $(OPENTITAN_BUILD_OUT_DIR) sw/device/examples/hello_world/hello_world_export_sim_verilator
 
 opentitan_sw_bootrom: $(OPENTITAN_BUILD_OUT_DIR)
-	cd $(OPENTITAN_SRC_DIR); \
 	ninja -C $(OPENTITAN_BUILD_OUT_DIR) \
 		sw/device/boot_rom/boot_rom_export_sim_verilator \
 		sw/device/boot_rom/boot_rom_export_sim_dv \
 		sw/device/boot_rom/boot_rom_export_fpga_nexysvideo
 
+## Build and run host based opentitan unittests
+opentitan_sw_test: $(OPENTITAN_BUILD_OUT_DIR)
+	ninja -C $(OPENTITAN_BUILD_OUT_DIR) test
+
 ## Removes only the OpenTitan build artifacts from out/
 opentitan_sw_clean:
 	rm -rf $(OPENTITAN_BUILD_DIR)
 
-.PHONY:: opentitan_sw_clean opentitan_sw_helloworld opentitan_sw_all opentitan_sw_bootrom
+.PHONY:: opentitan_sw_clean opentitan_sw_helloworld opentitan_sw_all opentitan_sw_bootrom opentitan_sw_test
