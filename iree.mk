228aa21441b11f502cac44ad00d5cafa6c6e8abb
Lun Dong <lundong@google.com>
2021-12-03 23:04:58 +0000

    Unpin the temporary snapshot and resync to the latest
    Previously we temporarily pinned to snapshot 665 due to b/208112327.
    
    Now the upstream bug has been fixed so that we can resync to the latest
    snapshot.
    
    BUG: 208112327
    
    Change-Id: I0a0c1e8efba55b93b139a6007bfb8afbf8e60bf0
    GitOrigin-RevId: 7bf15115bd49f4e4c989d11c1018dd8ed28dc2f4

--- iree.mk
+++ iree.mk
@@ -44,9 +44,8 @@ $(IREE_COMPILER_DIR):
 # the `iree_runtime` target. The outputs of this target are placed in
 # out/host/iree_compiler.
 #
-# TODO(b/208112327): unpin the compiler.
 iree_compiler: | $(IREE_COMPILER_DIR)
-	scripts/download_iree_compiler.py --tag snapshot-20211122.665
+	scripts/download_iree_compiler.py
 
 iree_commit_check:
 	scripts/check-iree-commit.sh $(IREE_SRC)
