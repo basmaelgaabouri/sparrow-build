224551cf34992a61b040091693b6c12229322a7e
Cindy Liu <hcindyl@google.com>
2022-01-26 15:28:02 -0800

    build: Update renode_src target
    Fetch the renode submodules only when we need to build renode from the
    source.
    
    Change-Id: I501c44a8b7fda9895b3291b84095b9290fa67565
    GitOrigin-RevId: 9ae6ae4df2a95cbfa3d4741ce1c5dc2d96560a13

--- renode.mk
+++ renode.mk
@@ -10,7 +10,7 @@ $(RENODE_OUT_DIR):
 
 $(RENODE_BIN): | $(RENODE_SRC_DIR) $(RENODE_OUT_DIR)
 	cd $(RENODE_SRC_DIR); \
-		./build.sh -d --skip-fetch -o $(RENODE_OUT_DIR)
+		./build.sh -d -o $(RENODE_OUT_DIR)
 	echo -e "commit_sha: $(RENODE_COMMIT)\n" > $(RENODE_OUT_DIR)/tag
 
 ## Builds the Renode system simulator
