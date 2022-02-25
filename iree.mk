092f11c3edc404f5eea88070273cc01ae4ec3aef
Lun Dong <lundong@google.com>
2022-02-25 17:41:13 +0000

    Merge "Revert "build: Temporarily pin the IREE compiler version to candidate 54""
    GitOrigin-RevId: e1f4cf61238e52695f7d37126636c4070789b1ae

--- iree.mk
+++ iree.mk
@@ -46,8 +46,7 @@ $(IREE_COMPILER_DIR):
 # out/host/iree_compiler.
 #
 iree_compiler: | $(IREE_COMPILER_DIR)
-# TODO(b/220902834) Temporarily pin to the earlier release 54
-	scripts/download_iree_compiler.py --tag candidate-20220220.54
+	scripts/download_iree_compiler.py
 iree_commit_check:
 	scripts/check-iree-commit.sh $(IREE_SRC)
 
