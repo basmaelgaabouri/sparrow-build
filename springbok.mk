998f72c54866c7c535a38bead4a09e99cc219192
Cindy Liu <hcindyl@google.com>
2022-03-18 16:46:24 +0000

    Merge "build: Add springbok scalar build target"
    GitOrigin-RevId: 36555f0d357993e1858703df87ee42f4d7c41844

--- springbok.mk
+++ springbok.mk
@@ -1,8 +1,16 @@
-SPRINGBOK_BUILD_DIR:=$(OUT)/springbok/rvv
-SPRINGBOK_SRC_DIR:=$(ROOTDIR)/sw/vec
+SPRINGBOK_BUILD_DIR := $(OUT)/springbok/rvv
+SPRINGBOK_SRC_DIR   := $(ROOTDIR)/sw/vec
+SPRINGBOK_BUILD_SCALAR_DIR := $(OUT)/springbok/scalar
 
 $(SPRINGBOK_BUILD_DIR)/build.ninja:
 	cmake -B $(SPRINGBOK_BUILD_DIR) -GNinja $(SPRINGBOK_SRC_DIR)
+
+# Set up BUILD_SIMPLIFIELD_CORE CMake option to remove usages of unimplemented
+# ISA extentions, such as f-ext.
+$(SPRINGBOK_BUILD_SCALAR_DIR)/build.ninja:
+	cmake -B $(SPRINGBOK_BUILD_SCALAR_DIR) -GNinja \
+		-DBUILD_SIMPLIFIED_CORE=ON \
+		$(SPRINGBOK_SRC_DIR)
 
 ## Vector core BSP and RVV test code
 #
@@ -14,7 +22,17 @@ springbok: $(SPRINGBOK_BUILD_DIR)/build.ninja
 test_springbok: springbok
 	cd $(SPRINGBOK_BUILD_DIR) && ctest -j 16
 
-clean_springbok:
-	@rm -rf $(SPRINGBOK_BUILD_DIR)
+## Springbok test code with simplified configuration
+#
+# This target builds the springbok SW artifacts with scalar-only configuration.
+# Source code is in sw/vec, while the output is placed in out/springbok/scalar.
+springbok_simplified: $(SPRINGBOK_BUILD_SCALAR_DIR)/build.ninja
+	cmake --build $(SPRINGBOK_BUILD_SCALAR_DIR)
 
-.PHONY:: springbok clean_springbok test_springbok
+springbok_clean:
+	rm -rf $(SPRINGBOK_BUILD_DIR)
+
+springbok_simplified_clean:
+	rm -rf $(SPRINGBOK_BUILD_SCALAR_DIR)
+
+.PHONY:: springbok springbok_clean test_springbok springbok_simplified springbok_simplified_clean
