cbb610855bd77585658c5469a0278caa30f7fb4d
Cindy Liu <hcindyl@google.com>
2022-06-16 19:46:53 -0700

    build: Bump kata rust cargo version to 2021-11-05 nightly
    Bump kata cargo version to 1.58 nightly
    
    See the build error for the old 1.56 nightly in
    https://louhi.dev/?projectId=5958780716318720#/execution-detail/4537583202992128
    
    Change-Id: I3e02252c6d2e730538cdd62093ccc9ec5fcfbeab
    GitOrigin-RevId: 2884684d1fdabf6e05ea680eafa1c8691f1a6e0a

--- setup.sh
+++ setup.sh
@@ -62,7 +62,7 @@ export PATH="${OUT}/host/flatbuffers/bin:${PATH}"
 export PATH="${OUT}/host/verilator/bin:${PATH}"
 export PATH="${OUT}/host/verible/bin:${PATH}"
 
-export KATA_RUST_VERSION="nightly-2021-08-05"
+export KATA_RUST_VERSION="nightly-2021-11-05"
 export RENODE_PORT=1234
 
 function renode
