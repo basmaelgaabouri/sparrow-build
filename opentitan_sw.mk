b9444de129e675450c0dc3dada7160e4ac3018bc
Yen-Kai Wang <ykwang@google.com>
2022-07-13 14:31:40 -0500

    Replace Ninja with Bazel to compile opentitan repo.
    We could generate helloworld image and run the helloworld example.
    The output message is logged in uart0.log.
    
    Change-Id: Ic11cded29d9a66791522ee0f19a854ea42e6f692
    GitOrigin-RevId: 24cd2d3ad1ec9989c19a375ba4c1f58e4abe075b

--- opentitan_sw.mk
+++ opentitan_sw.mk
@@ -1,55 +1,99 @@
 OPENTITAN_SRC_DIR             := $(ROOTDIR)/hw/opentitan-upstream
 OPENTITAN_BUILD_DIR           := $(OUT)/opentitan/sw
 OPENTITAN_BUILD_OUT_DIR       := $(OPENTITAN_BUILD_DIR)/build-out
-OPENTITAN_BUILD_NINJA         := $(OPENTITAN_BUILD_OUT_DIR)/build.ninja
-OPENTITAN_BUILD_SW_DEVICE_DIR := $(OPENTITAN_BUILD_DIR)/build-bin/sw/device
+OPENTITAN_BUILD_SW_DIR        := $(OPENTITAN_BUILD_DIR)/build-out/sw
+OPENTITAN_BUILD_SW_DEVICE_DIR := $(OPENTITAN_BUILD_DIR)/build-out/sw/device
+OPENTITAN_BUILD_SW_DEVICE_TESTS_DIR := $(OPENTITAN_BUILD_DIR)/build-out/sw/device/tests
+OPENTITAN_BUILD_LOG_DIR       := $(OPENTITAN_BUILD_DIR)/build-log
+OPENTITAN_BUILD_LOG_SW_DIR    := $(OPENTITAN_BUILD_DIR)/build-log/sw
+
+OPENTITAN_BAZEL_BIN_PATH := $(shell cd $(OPENTITAN_SRC_DIR) && bazel info bazel-bin)
+OPENTITAN_BAZEL_OUTPUT_PATH := $(shell cd $(OPENTITAN_SRC_DIR) && bazel info output_path)
+OPENTITAN_BAZEL_LOG_PATH := $(shell cd $(OPENTITAN_SRC_DIR) && bazel info bazel-testlogs)
 
 $(OPENTITAN_BUILD_OUT_DIR):
 	@echo "Creating output directory $(OPENTITAN_BUILD_DIR)"
 	@mkdir -p "$(OPENTITAN_BUILD_DIR)"
-
-$(OPENTITAN_BUILD_NINJA): | $(OPENTITAN_BUILD_OUT_DIR)
-	cd $(OPENTITAN_SRC_DIR); \
-		TOOLCHAIN_PATH=$(CACHE)/toolchain \
-		BUILD_ROOT=$(OPENTITAN_BUILD_DIR) \
-		CC_FOR_BUILD=gcc-10 \
-		CXX_FOR_BUILD=g++-10 ./meson_init.sh -f;
+	@mkdir -p "$(OPENTITAN_BUILD_SW_DIR)"
+	@mkdir -p "$(OPENTITAN_BUILD_SW_DEVICE_DIR)"
+	@mkdir -p "$(OPENTITAN_BUILD_SW_DEVICE_TESTS_DIR)"
 
 ## Builds the hardware testing binaries from OpenTitan in hw/opentitan-upstream
 # The output is stored at out/opentitan/sw/build-bin
-opentitan_sw_all: $(OPENTITAN_BUILD_NINJA)
-	ninja -C $(OPENTITAN_BUILD_OUT_DIR) all
+opentitan_sw_all: | $(OPENTITAN_BUILD_OUT_DIR) \
+                  opentitan_sw_verilator_sim
+	cd $(OPENTITAN_SRC_DIR) && \
+		bazel query "kind(test, //sw/device/tests:all)" | \
+			grep "_fpga" | \
+			xargs bazel build  --define DISABLE_VERILATOR_BUILD=true && \
+		find "bazel-out/" -type f -wholename "*fastbuild-*/sw/device/tests/*.bin" | \
+			sed 's/\.bin//g' | \
+			xargs -I {} cp -f {} "$(OPENTITAN_BUILD_SW_DEVICE_TESTS_DIR)/"
+
+$(OPENTITAN_BUILD_SW_DEVICE_DIR)/examples/hello_world: | $(OPENTITAN_BUILD_OUT_DIR)
+	@mkdir -p "$(OPENTITAN_BUILD_SW_DEVICE_DIR)/examples/hello_world"
 
 ## Build the sw helloworld ELF from hw/opentitan-upstream
-# The output is stored at out/opentitan/sw/build-bin/sw/device/examples/hello_world
-opentitan_sw_helloworld: $(OPENTITAN_BUILD_NINJA)
-	ninja -C $(OPENTITAN_BUILD_OUT_DIR) \
-		sw/device/examples/hello_world/hello_world_export_sim_verilator
+# The output is stored at out/opentitan/sw/build-out/sw/device/examples/hello_world
+opentitan_sw_helloworld: | $(OPENTITAN_BUILD_SW_DEVICE_DIR)/examples/hello_world
+	cd $(OPENTITAN_SRC_DIR) && \
+		bazel build //sw/device/examples/hello_world:hello_world
+	find $(OPENTITAN_BAZEL_OUTPUT_PATH) -name "hello_world*.elf" \
+		-exec cp -f '{}' "$(OPENTITAN_BUILD_SW_DEVICE_DIR)/examples/hello_world" \;
+
+$(OPENTITAN_BUILD_SW_DEVICE_DIR)/boot_rom: | $(OPENTITAN_BUILD_OUT_DIR)
+	@mkdir -p "$(OPENTITAN_BUILD_SW_DEVICE_DIR)/boot_rom"
+
+## Build the boot rom artifacts for Open Titan from hw/opentitan-upstream
+# The artifacts are stored at out/opentitan/sw/build-out/sw/device/boot_rom
+# TODO(ykwang): revise boot_rom to test_rom.
+opentitan_sw_bootrom: | $(OPENTITAN_BUILD_SW_DEVICE_DIR)/boot_rom
+	cd $(OPENTITAN_SRC_DIR) && \
+		bazel build //sw/device/lib/testing/test_rom:test_rom
+	find $(OPENTITAN_BAZEL_OUTPUT_PATH) -name "test_rom_sim_verilator.scr.39.vmem" \
+		-exec cp -f '{}' "$(OPENTITAN_BUILD_SW_DEVICE_DIR)/boot_rom/boot_rom_sim_verilator.scr.39.vmem" \;
+
+$(OPENTITAN_BUILD_SW_DEVICE_DIR)/otp_img: | $(OPENTITAN_BUILD_OUT_DIR)
+	@mkdir -p "$(OPENTITAN_BUILD_SW_DEVICE_DIR)/otp_img"
+
+## Build the opt image for Open Titan from hw/opentitan-upstream
+# The artifacts are stored at out/opentitan/sw/build-out/sw/device/otp_img/otp_img_sim_verilator.vmem
+opentitan_opt_img: | $(OPENTITAN_BUILD_SW_DEVICE_DIR)/otp_img
+	cd $(OPENTITAN_SRC_DIR) && \
+		bazel build //hw/ip/otp_ctrl/data:img_dev
+	find $(OPENTITAN_BAZEL_BIN_PATH) -name "img_dev*.vmem" \
+		-exec cp -f '{}' "$(OPENTITAN_BUILD_SW_DEVICE_DIR)/otp_img/otp_img_sim_verilator.vmem" \;
 
 ## Build the Verilator sim SW for Open Titan from hw/opentitan-upstream
 # The artifacts include boot_rom, hello_world ELF, and otp image. The artifacts
 # are stored at out/opentitan/sw/build-bin
-opentitan_sw_verilator_sim: $(OPENTITAN_BUILD_NINJA)
-	ninja -C $(OPENTITAN_BUILD_OUT_DIR) \
-		sw/device/examples/hello_world/hello_world_export_sim_verilator \
-		sw/device/boot_rom/boot_rom_export_sim_verilator \
-		sw/device/otp_img/otp_img_export_sim_verilator
+opentitan_sw_verilator_sim: | $(OPENTITAN_BUILD_OUT_DIR) \
+                              opentitan_sw_helloworld \
+                              opentitan_sw_bootrom \
+                              opentitan_opt_img
 
-## Build the boot rom artifacts for Open Titan from hw/opentitan-upstream
-# The artifacts are stored at out/opentitan/sw/build-bin/sw/device/boot_rom
-opentitan_sw_bootrom: $(OPENTITAN_BUILD_NINJA)
-	ninja -C $(OPENTITAN_BUILD_OUT_DIR) \
-		sw/device/boot_rom/boot_rom_export_sim_verilator \
-		sw/device/boot_rom/boot_rom_export_sim_dv \
-		sw/device/boot_rom/boot_rom_export_fpga_nexysvideo
+$(OPENTITAN_BUILD_LOG_DIR):
+	@echo "Creating log directory $(OPENTITAN_BUILD_LOG_DIR)"
+	@mkdir -p "$(OPENTITAN_BUILD_LOG_DIR)"
+	@mkdir -p "$(OPENTITAN_BUILD_LOG_SW_DIR)"
 
 ## Build and run host based opentitan unittests
-# The artifacts are stored at out/opentitan/sw/build-bin/
-opentitan_sw_test: $(OPENTITAN_BUILD_NINJA)
-	ninja -C $(OPENTITAN_BUILD_OUT_DIR) test
+# The artifacts are stored at out/opentitan/sw/build-out/sw/device
+opentitan_sw_test: | $(OPENTITAN_BUILD_OUT_DIR) \
+                   $(OPENTITAN_BUILD_LOG_DIR)
+	cd $(OPENTITAN_SRC_DIR) && \
+		bazel query "kind(test, //sw/device/...)" | \
+			grep "_unittest" | \
+			xargs bazel test --build_tests_only=false \
+				--define DISABLE_VERILATOR_BUILD=true \
+				--test_tag_filters=-broken,-cw310,-verilator,-dv
+	cp -rf $(OPENTITAN_BAZEL_BIN_PATH)/sw/device $(OPENTITAN_BUILD_SW_DIR)
+	cp -rf $(OPENTITAN_BAZEL_LOG_PATH)/sw/device $(OPENTITAN_BUILD_LOG_SW_DIR)
+
 
 ## Removes only the OpenTitan build artifacts from out/opentitan/sw
 opentitan_sw_clean:
 	rm -rf $(OPENTITAN_BUILD_DIR)
 
-.PHONY:: opentitan_sw_clean opentitan_sw_helloworld opentitan_sw_all opentitan_sw_bootrom opentitan_sw_test opentitan_sw_verilator_sim
+.PHONY:: opentitan_sw_all opentitan_sw_clean opentitan_sw_verilator_sim opentitan_sw_test
+.PHONY:: opentitan_sw_helloworld opentitan_sw_bootrom opentitan_opt_img
