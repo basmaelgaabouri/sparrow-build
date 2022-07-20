bc6dfaf348adbedd42d107e51ad9b5126d0d2511
June Tate-Gans <jtgans@google.com>
2022-07-20 19:32:06 +0000

    Merge "build: Ensure bootstrap and tock clean targets actually clean"
    GitOrigin-RevId: 3e4b0b38be5d70b5d19e0f5431525f2af9562174

--- tock.mk
+++ tock.mk
@@ -17,16 +17,20 @@ MATCHA_BUNDLE_RELEASE := $(OUT)/matcha-bundle-release.elf
 $(RUSTDIR)/bin/elf2tab: | rust_presence_check
 	cargo install elf2tab --version 0.6.0
 
-$(MATCHA_BUNDLE_DEBUG): $(RUSTDIR)/bin/elf2tab | rust_presence_check
+$(MATCHA_APP_DEBUG): | rust_presence_check
 	cd $(MATCHA_PLATFORM_SRC_DIR); cargo build
 	cd $(MATCHA_APP_SRC_DIR); PLATFORM=opentitan cargo build
+
+$(MATCHA_APP_RELEASE): | rust_presence_check
+	cd $(MATCHA_PLATFORM_SRC_DIR); cargo build --release
+	cd $(MATCHA_APP_SRC_DIR); PLATFORM=opentitan cargo build --release
+
+$(MATCHA_BUNDLE_DEBUG): $(MATCHA_APP_DEBUG) $(RUSTDIR)/bin/elf2tab | rust_presence_check
 	elf2tab -n matcha -o $(MATCHA_APP_DEBUG).tab $(MATCHA_APP_DEBUG) --stack 4096 --app-heap 1024 --kernel-heap 1024 --protected-region-size=64
 	cp $(MATCHA_PLATFORM_DEBUG) $(MATCHA_BUNDLE_DEBUG)
 	riscv32-unknown-elf-objcopy --update-section .apps=$(MATCHA_APP_DEBUG).tbf $(MATCHA_BUNDLE_DEBUG)
 
-$(MATCHA_BUNDLE_RELEASE): $(RUSTDIR)/bin/elf2tab | rust_presence_check
-	cd $(MATCHA_PLATFORM_SRC_DIR); cargo build --release
-	cd $(MATCHA_APP_SRC_DIR); PLATFORM=opentitan cargo build --release
+$(MATCHA_BUNDLE_RELEASE): $(MATCHA_APP_RELEASE) $(RUSTDIR)/bin/elf2tab | rust_presence_check
 	elf2tab -n matcha -o $(MATCHA_APP_RELEASE).tab $(MATCHA_APP_RELEASE) --stack 4096 --app-heap 1024 --kernel-heap 1024 --protected-region-size=64
 	cp $(MATCHA_PLATFORM_RELEASE) $(MATCHA_BUNDLE_RELEASE)
 	riscv32-unknown-elf-objcopy --update-section .apps=$(MATCHA_APP_RELEASE).tbf $(MATCHA_BUNDLE_RELEASE)
@@ -41,7 +45,13 @@ matcha_tock_release: $(MATCHA_BUNDLE_RELEASE)
 
 ## Removes the TockOS and libtockrs build artifacts from out/
 matcha_tock_clean:
-	cd $(MATCHA_PLATFORM_SRC_DIR); make TARGET_DIRECTORY=$(OUT)/tock clean
+	cd $(MATCHA_PLATFORM_SRC_DIR); PLATFORM=opentitan cargo clean --target-dir=$(OUT)/matcha
 	cd $(MATCHA_APP_SRC_DIR); PLATFORM=opentitan cargo clean --target-dir=$(OUT)/matcha
+	rm -rf $(OUT)/matcha
+	rm -f $(MATCHA_BUNDLE_DEBUG) $(MATCHA_BUNDLE_RELEASE)
 
 .PHONY:: matcha_tock_debug matcha_tock_release matcha_tock_clean
+
+# Mark these real files as phony since building them requires calling out to a
+# separate build system (cargo)
+.PHONY:: $(MATCHA_APP_DEBUG) $(MATCHA_APP_RELEASE)
