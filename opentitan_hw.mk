5062a6666c65e51a53614f2d1edfc11f4a944d38
Cindy Liu <hcindyl@google.com>
2022-09-16 18:55:52 +0000

    Merge "Add more opentitan verilated peripheral tests"
    GitOrigin-RevId: 307ad50c2141a171d106795eb4ca91196b2cfc3b

--- opentitan_hw.mk
+++ opentitan_hw.mk
@@ -8,4 +8,15 @@ earlgrey_hw_verilator_smoketests: | $(VERILATOR_BIN) $(OPENTITAN_BUILD_LOG_DIR)
 	cd $(OPENTITAN_SRC_DIR) && \
 		cp -rf "bazel-testlogs/sw/device" "$(OPENTITAN_BUILD_LOG_SW_DIR)"
 
-.PHONY:: earlgrey_hw_verilator_smoketests
+## Run All the OpenTitan earlgrey device tests in verilator
+# Build the verilated model, the test software and runs the tests.
+#
+# Note: This will take hours to finish. Run it with caution.
+earlgrey_hw_verilator_tests_all: | $(VERILATOR_BIN) $(OPENTITAN_BUILD_LOG_DIR)
+	cd $(OPENTITAN_SRC_DIR) && \
+		bazel query "kind(test, //sw/device/tests:all)" \
+			| xargs bazel test  --test_tag_filters=verilator,-broken --test_output=streamed
+	cd $(OPENTITAN_SRC_DIR) && \
+		cp -rf "bazel-testlogs/sw/device" "$(OPENTITAN_BUILD_LOG_SW_DIR)"
+
+.PHONY:: earlgrey_hw_verilator_smoketests earlgrey_hw_verilator_tests_all
