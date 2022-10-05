646dd9a3dec6a73ef21a06013a1d6013b03ba949
Sam Leffler <sleffler@google.com>
2022-10-05 17:13:04 +0000

    Merge "build: add copyright notices"
    GitOrigin-RevId: 8675facc8182c4ab41b22cf236a9d494a199f34d

--- tock.mk
+++ tock.mk
@@ -1,3 +1,17 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Matcha platform + kernel binaries
 MATCHA_PLATFORM_SRC_DIR := $(ROOTDIR)/sw/matcha/platform/
 MATCHA_PLATFORM_DEBUG   := $(OUT)/matcha/riscv32imc-unknown-none-elf/debug/matcha_platform
