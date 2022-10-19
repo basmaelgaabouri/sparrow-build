d0cf744a9c97eba083f4b60f7a7808202d73fde7
Austin Appleby <aappleby@google.com>
2022-10-19 02:05:45 +0000

    Add a build rule for the Puppeteer boot rom.
    Change-Id: I120c7a00f53179943d8a4b2895e7cf6cd82a3e2b
    GitOrigin-RevId: da179c4809574e637a869c91161e3a4a98b8cd5c

--- sparrow_boot_rom.mk
+++ sparrow_boot_rom.mk
@@ -16,6 +16,8 @@ SPARROW_BOOT_ROM_SOURCE_DIR:=$(ROOTDIR)/sw/multihart_boot_rom
 SPARROW_BOOT_ROM_BUILD_DIR:=$(OUT)/sparrow_boot_rom
 SPARROW_BOOT_ROM_BUILD_NINJA_SCRIPT:=$(SPARROW_BOOT_ROM_BUILD_DIR)/build.ninja
 SPARROW_BOOT_ROM_ELF:=multihart_boot_rom.elf
+PUPPETEER_BOOT_ROM_ELF:=$(SPARROW_BOOT_ROM_BUILD_DIR)/puppeteer_boot_rom.elf
+PUPPETEER_BOOT_ROM_SCR_VMEM:=$(SPARROW_BOOT_ROM_BUILD_DIR)/puppeteer_boot_rom.scr.vmem
 
 $(SPARROW_BOOT_ROM_BUILD_DIR):
 	@mkdir -p "$(SPARROW_BOOT_ROM_BUILD_DIR)"
@@ -35,5 +37,31 @@ multihart_boot_rom: $(SPARROW_BOOT_ROM_BUILD_NINJA_SCRIPT)
 multihart_boot_rom_clean:
 	rm -rf $(SPARROW_BOOT_ROM_BUILD_DIR)
 
+$(MATCHA_OUT_DIR)/data/autogen:
+	@mkdir -p $(MATCHA_OUT_DIR)/data/autogen
 
-.PHONY:: multihart_boot_rom multihart_boot_rom_clean
+$(MATCHA_OUT_DIR)/data/autogen/top_matcha.gen.hjson: | $(MATCHA_OUT_DIR)/data/autogen
+	PYTHONPATH=$(OPENTITAN_SRC_DIR)/util ${MATCHA_SRC_DIR}/util/topgen_matcha.py \
+			-t $(MATCHA_SRC_DIR)/hw/top_matcha/data/top_matcha.hjson --no-top \
+			-o $(MATCHA_OUT_DIR) --dump_gen_hjson
+
+$(PUPPETEER_BOOT_ROM_ELF):
+	$(MAKE) -C "$(SPARROW_BOOT_ROM_SOURCE_DIR)" puppeteer_boot_rom
+
+$(PUPPETEER_BOOT_ROM_SCR_VMEM): $(PUPPETEER_BOOT_ROM_ELF) $(MATCHA_OUT_DIR)/data/autogen/top_matcha.gen.hjson
+	cd $(OPENTITAN_SRC_DIR) && bazel run //hw/ip/rom_ctrl/util:scramble_image \
+			$(MATCHA_OUT_DIR)/data/autogen/top_matcha.gen.hjson $(PUPPETEER_BOOT_ROM_ELF) $@
+
+## Build the Puppeteer boot ROM image
+#
+# This builds an *insecure*, test-only bootrom that allows a client to
+# read/write arbitrary memory via a simple UART console.
+puppeteer_boot_rom_elf: $(PUPPETEER_BOOT_ROM_ELF)
+
+## Build the Puppeteer boot ROM image and scramble it for Verilator simulation.
+#
+# Same as above, but applies a scrambling pass to enable compatibility with
+# the secure SRAM configuration used in Verilator.
+puppeteer_boot_rom_scr_vmem: $(PUPPETEER_BOOT_ROM_SCR_VMEM)
+
+.PHONY:: multihart_boot_rom multihart_boot_rom_clean puppeteer_boot_rom_elf puppeteer_boot_rom_scr_vmem
