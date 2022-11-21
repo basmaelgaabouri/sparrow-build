c1b2df7dbf21a9fbcf3a77fb536a5c4f3d221b61
June Tate-Gans <jtgans@google.com>
2022-11-21 13:00:26 -0600

    cantrip: Step 3: adjust build paths, variables, and targets
    Change-Id: I98b7ebe0a0e16f730aa36f5773a6f3b87c09c459
    GitOrigin-RevId: 56829c3692e5497e65bd11ee13a5afd51a21f547

--- platforms/sparrow/cantrip_builtins.mk
+++ platforms/sparrow/cantrip_builtins.mk
@@ -12,20 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-CANTRIP_APPS_RELEASE   := $(CANTRIP_OUT_C_APP_RELEASE)/hello/hello.app \
-                       $(CANTRIP_OUT_RUST_APP_RELEASE)/fibonacci/fibonacci.app \
-                       $(CANTRIP_OUT_RUST_APP_RELEASE)/keyval/keyval.app \
-                       $(CANTRIP_OUT_RUST_APP_RELEASE)/logtest/logtest.app \
-                       $(CANTRIP_OUT_RUST_APP_RELEASE)/panic/panic.app \
-                       $(CANTRIP_OUT_C_APP_RELEASE)/suicide/suicide.app \
-                       $(CANTRIP_OUT_RUST_APP_RELEASE)/timer/timer.app
-CANTRIP_MODEL_RELEASE  := $(OUT)/springbok_iree/quant_models/mobilenet_v1_emitc_static.model
+CANTRIP_APPS_RELEASE  := $(CANTRIP_OUT_C_APP_RELEASE)/hello/hello.app \
+                         $(CANTRIP_OUT_RUST_APP_RELEASE)/fibonacci/fibonacci.app \
+                         $(CANTRIP_OUT_RUST_APP_RELEASE)/keyval/keyval.app \
+                         $(CANTRIP_OUT_RUST_APP_RELEASE)/logtest/logtest.app \
+                         $(CANTRIP_OUT_RUST_APP_RELEASE)/panic/panic.app \
+                         $(CANTRIP_OUT_C_APP_RELEASE)/suicide/suicide.app \
+                         $(CANTRIP_OUT_RUST_APP_RELEASE)/timer/timer.app
+CANTRIP_MODEL_RELEASE := $(OUT)/springbok_iree/quant_models/mobilenet_v1_emitc_static.model
 
-CANTRIP_APPS_DEBUG     := $(CANTRIP_OUT_C_APP_DEBUG)/hello/hello.app \
-                       $(CANTRIP_OUT_RUST_APP_DEBUG)/fibonacci/fibonacci.app \
-                       $(CANTRIP_OUT_RUST_APP_DEBUG)/keyval/keyval.app \
-                       $(CANTRIP_OUT_RUST_APP_DEBUG)/logtest/logtest.app \
-                       $(CANTRIP_OUT_RUST_APP_RELEASE)/panic/panic.app \
-                       $(CANTRIP_OUT_C_APP_DEBUG)/suicide/suicide.app \
-                       $(CANTRIP_OUT_RUST_APP_DEBUG)/timer/timer.app
-CANTRIP_MODEL_DEBUG    := $(OUT)/springbok_iree/quant_models/mobilenet_v1_emitc_static.model
+CANTRIP_APPS_DEBUG    := $(CANTRIP_OUT_C_APP_DEBUG)/hello/hello.app \
+                         $(CANTRIP_OUT_RUST_APP_DEBUG)/fibonacci/fibonacci.app \
+                         $(CANTRIP_OUT_RUST_APP_DEBUG)/keyval/keyval.app \
+                         $(CANTRIP_OUT_RUST_APP_DEBUG)/logtest/logtest.app \
+                         $(CANTRIP_OUT_RUST_APP_RELEASE)/panic/panic.app \
+                         $(CANTRIP_OUT_C_APP_DEBUG)/suicide/suicide.app \
+                         $(CANTRIP_OUT_RUST_APP_DEBUG)/timer/timer.app
+CANTRIP_MODEL_DEBUG   := $(OUT)/springbok_iree/quant_models/mobilenet_v1_emitc_static.model
