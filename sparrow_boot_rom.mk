8e224b41189194767b075f74daa8448b07ba7fc9
Cindy Liu <hcindyl@google.com>
2023-02-06 14:59:56 -0800

    build: Remove explicit puppeteer rom target
    It is integrated into the matcha flow and spliced into the FPGA
    bitstream.
    
    Change-Id: I94007196a1d7ca8b5c3afc558e779c4650fbe8e4
    GitOrigin-RevId: be78b8e181b3724324b6d0e7641ef291ee362ce5

--- sparrow_boot_rom.mk
+++ sparrow_boot_rom.mk
@@ -35,27 +35,5 @@ multihart_boot_rom: $(SPARROW_BOOT_ROM_BUILD_NINJA_SCRIPT)
 multihart_boot_rom_clean:
 	rm -rf $(SPARROW_BOOT_ROM_BUILD_DIR)
 
-$(MATCHA_OUT_DIR)/data/autogen:
-	@mkdir -p $(MATCHA_OUT_DIR)/data/autogen
 
-$(MATCHA_OUT_DIR)/data/autogen/top_matcha.gen.hjson: | $(MATCHA_OUT_DIR)/data/autogen
-	PYTHONPATH=$(OPENTITAN_SRC_DIR)/util ${MATCHA_SRC_DIR}/util/topgen_matcha.py \
-			-t $(MATCHA_SRC_DIR)/hw/top_matcha/data/top_matcha.hjson --no-top \
-			-o $(MATCHA_OUT_DIR) --dump_gen_hjson
-
-## Build the Puppeteer boot ROM image and scramble it for Verilator simulation.
-#
-# This builds an *insecure*, test-only bootrom that allows a client to
-# read/write arbitrary memory via a simple UART console.
-# Puppeteer currently does _not_ work in Renode as it now requires manipulating
-# hardware registers that don't exist in Renode on bootup in order to work
-# correctly on our FPGA platforms. Once those peripherals are stubbed out in
-# Renode, Puppeteer should work there again.
-puppeteer_boot_rom_scr_vmem: $(MATCHA_OUT_DIR)/data/autogen/top_matcha.gen.hjson $(SPARROW_BOOT_ROM_BUILD_NINJA_SCRIPT)
-	cmake --build $(SPARROW_BOOT_ROM_BUILD_DIR) --target puppeteer_boot_rom.elf
-	cd $(OPENTITAN_SRC_DIR) && bazel run //hw/ip/rom_ctrl/util:scramble_image \
-			$(MATCHA_OUT_DIR)/data/autogen/top_matcha.gen.hjson \
-			$(SPARROW_BOOT_ROM_BUILD_DIR)/puppeteer_boot_rom.elf \
-			$(SPARROW_BOOT_ROM_BUILD_DIR)/puppeteer_boot_rom.scr.39.vmem
-
-.PHONY:: multihart_boot_rom multihart_boot_rom_clean puppeteer_boot_rom_scr_vmem
+.PHONY:: multihart_boot_rom multihart_boot_rom_clean
